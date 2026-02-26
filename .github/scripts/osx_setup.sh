84ceabcae42276c23ac70e3ce2a2098df7065bc9
John R Patek Sr <johnpatek2@gmail.com>
2026-02-26 13:13:54 -0600

    trying http cache

--- .github/scripts/osx_setup.sh
+++ .github/scripts/osx_setup.sh
@@ -1,2 +1,2 @@
 find /Applications/Xcode_* -maxdepth 0 -type d ! -name 'Xcode_${{ env.XC_VERSION }}.app' -exec rm -rf {} \;
-brew install autoconf autoconf-archive automake libtool
+brew install autoconf autoconf-archive automake libtool cmake make
