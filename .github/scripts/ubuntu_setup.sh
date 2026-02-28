eb5e570e613eabbd03099bbc9a803ed49c497250
John R Patek Sr <johnpatek2@gmail.com>
2026-02-27 19:22:52 -0600

    testing linux build again

--- .github/scripts/ubuntu_setup.sh
+++ .github/scripts/ubuntu_setup.sh
@@ -27,26 +27,9 @@
 # causing unpredictable behavior and build failures.
 #
 echo "=============================================================================="
-echo "Freeing up disk space on CI system"
+echo "Installing Ubuntu dependencies"
 echo "=============================================================================="
 
-echo "Listing 100 largest packages"
-dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
-df -h
-echo "Removing large packages"
-sudo apt-get remove -y '^ghc-8.*'
-sudo apt-get remove -y '^dotnet-.*'
-sudo apt-get remove -y '^llvm-.*'
-sudo apt-get remove -y 'php.*'
-sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
-sudo apt-get autoremove -y
-sudo apt-get clean
-echo "Removing large directories"
-# deleting 15GB
-rm -rf /usr/share/dotnet/
-rm -rf /opt/hostedtoolcache
-rm -rf /opt/ghc
-sudo rm -rf /usr/local/lib/android
 df -h
 sudo apt-get update
 sudo apt-get install -y \
@@ -79,5 +62,6 @@ sudo apt-get install -y \
   libxtst-dev \
   libxkbcommon-dev \
   libxkbcommon-x11-dev \
-  libxrender-dev
+  libxrender-dev \
+  libxrandr-dev
   df -h
